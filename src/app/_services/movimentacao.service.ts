eac07a856fc8dc15bb2bc2605b5c3578ecebe08f
Marciano Pasa <pasamarciano@edu.unisinos.br>
2023-09-24 16:43:38 -0300

    Edição e exclusão da movimentação

--- src/app/_services/movimentacao.service.ts
+++ src/app/_services/movimentacao.service.ts
@@ -10,20 +10,20 @@ export class MovimentacaoService {
     constructor(private http: HttpClient) { }
 
     getAll() {
-        return this.http.get<Movimentacao[]>(`${environment.apiUrl}/movimentacao`);
+      return this.http.get<Movimentacao[]>(`${environment.apiUrl}/movimentacao`);
     }
 
     create(data: any): Observable<any> {
       return this.http.post(`${environment.apiUrl}/movimentacao`, data);
     }
 
-    edit(data: any): Observable<any> {
-      console.log("DATA_ID: " + data.id);
-      return this.http.put(`${environment.apiUrl}/movimentacao/${data.id}`, data);
-    }
-
     delete(data: any): Observable<any> {
       console.log("DATA_ID: " + data.id);
       return this.http.delete(`${environment.apiUrl}/movimentacao/${data.id}`, { responseType: 'text' });
+    }
+
+    edit(data: any): Observable<any> {
+      console.log("DATA_ID: " + data.id);
+      return this.http.put(`${environment.apiUrl}/movimentacao/${data.id}'}`, data);
     }
 }
